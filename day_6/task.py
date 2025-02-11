c654c12f59b8766cb343602957a0d1829b9e13e3
ACEKaito1412 <83101104+ACEKaito1412@users.noreply.github.com>
2025-02-11 16:19:12 +0000

    day 6

---
+++ day_6/task.py
@@ -0,0 +1,40 @@
+"""
+    - defining a function
+    - calling a functions
+    - new loop which is while loop
+    - using while loop with conditions
+"""
+
+# Reeborg's World Hurdle Four My Created Solution:
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    while(wall_on_right()):
+        move()
+    turn_right()
+    move()
+    if(right_is_clear()):
+        turn_right()
+        move()
+    while(wall_on_right()):
+        if(wall_in_front()):
+            break
+        move()
+    turn_left()
+    
+        
+state = True;
+while(state):
+    if(at_goal()):
+        state = False
+    elif(front_is_clear()):
+        move()
+    elif(wall_in_front()):
+        turn_left()
+        jump()
+"""    
+       
