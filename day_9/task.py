2853e318925323264c4f43c2739f4885f2d954ac
ACEKaito1412 <jhuncarlomacdon@gmail.com>
2025-02-14 23:48:43 +0800

    day 9 - partial

---
+++ day_9/task.py
@@ -0,0 +1,17 @@
+"""
+    - dictionary
+    - key value pair
+    - wiping a dictionary
+    - editing the data
+"""
+
+programming_dictionary =  {
+    "Bug" : "An error on the program",
+    "Function" : "A piece of code that you can call",
+    "Loop" : "Doing something over and over again"
+}
+
+print(programming_dictionary["Bug"])
+
+for key in programming_dictionary:
+    print(programming_dictionary[key])
