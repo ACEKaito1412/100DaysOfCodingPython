0c439d822d94c4ae6986d1d140b4785c07f120fd
ACEKaito1412 <jhuncarlomacdon@gmail.com>
2025-02-26 22:44:48 +0800

    Day 20

---
+++ day_20/day.md
@@ -0,0 +1,2 @@
+# Day 20
+    - creating a snake game using turtle part_1
---
+++ day_20/snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+UP = 90
+LEFT = 180
+DOWN = 270
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.snake_list = []
+        self.state = "left"
+        self.setup_snake()
+        self.head = self.snake_list[0]
+
+    def setup_snake(self):
+        for i in range(0, 3):
+            n = Turtle('square')
+            n.penup()
+            n.setpos(i * -20, 0)
+            n.color('white')
+            self.snake_list.append(n)
+
+    def move(self):
+        for index in range(len(self.snake_list) - 1, 0, -1):
+            x = self.snake_list[index - 1].xcor()
+            y = self.snake_list[index - 1].ycor()
+            self.snake_list[index].goto(x, y)
+
+        self.snake_list[0].forward(20)
+    
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.snake_list[0].setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.snake_list[0].setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.snake_list[0].setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.snake_list[0].setheading(RIGHT)
+        
