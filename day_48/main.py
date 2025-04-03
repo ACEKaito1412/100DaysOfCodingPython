b27aba3714d13c068303722118244781fc6f5228
ACEKaito1412 <jhuncarlomacdon@gmail.com>
2025-04-03 23:20:07 +0800

    day 48, using selenium part 1

---
+++ day_48/main.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+#  Keep browser open
+chrome_option = webdriver.EdgeOptions()
+chrome_option.add_experimental_option("detach", True)
+
+driver = webdriver.Edge(options=chrome_option)
+driver.get("https://appbrewery.github.io/instant_pot/")
+
+
+# price_whole = driver.find_element(by=By.CLASS_NAME, value="a-price-whole")
+# price_fraction = driver.find_element(by=By.CLASS_NAME, value="a-price-fraction")
+
+# print(f"the price is {price_whole.text}.{price_fraction.text}")
+
+# driver.find_element(By.NAME, "fname") # useful for finding inputs in form
+# driver.find_element(By.ID, "submitBtn") #finding element using there idd
+
+# driver.find_element(By.CSS_SELECTOR, ".demo_class a") # using css selector
+
+# driver.close() this will close the tab that is open
+driver.quit() # this will close the browser
