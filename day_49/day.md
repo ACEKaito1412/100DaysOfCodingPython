ecff19e006e4ed3fbc194e6563bf4d7912450839
ACEKaito1412 <jhuncarlomacdon@gmail.com>
2025-04-07 23:33:08 +0800

    Day 49 Apply Jobs Automation

---
+++ day_49/day.md
@@ -0,0 +1,5 @@
+# Day 49
+    - its day 49, well
+    - creating a bot to automate job application
+    - using selenium on applying jobs on linkedin
+    
--- day_49/main.py
+++ day_49/main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from dotenv import load_dotenv
 import os
 import time as t
+import random
 
 load_dotenv()
 
@@ -20,25 +21,27 @@ chrome_options.add_experimental_option("detach", True)
 chrome_driver = webdriver.Chrome(chrome_options)
 chrome_driver.get(url=url)
 
-t.sleep(5)
 
-wait = WebDriverWait(chrome_driver, 15)
+
+t.sleep(random.uniform(5,9))
+
+wait = WebDriverWait(chrome_driver, random.uniform(5, 9))
 sign_in_btn = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "sign-in-modal__outlet-btn")))
 
 state = True
 
+#  sometimes button is cant be click
 try:
     sign_in_btn.click()
 except Exception as e:
     state = False
+    print("Button Cant be click")
 
-    modal_dismiss = chrome_driver.find_element(by=By.CLASS_NAME, value="modal__dismiss")
-    modal_dismiss.click()
-    print(e.args)
-
-t.sleep(5)
+# wait for the page to load
+t.sleep(4)
 
 if state:
+    # input email and password
     email_input = chrome_driver.find_element(By.NAME, value="session_key")
     email_input.send_keys(email, Keys.ENTER)
 
@@ -46,7 +49,7 @@ if state:
     pass_input.send_keys(password, Keys.ENTER)
 
 
-    t.sleep(5)
+    t.sleep(random.uniform(5, 9))
 
     easy_apply_btn = chrome_driver.find_element(By.ID, value="jobs-apply-button-id")
     easy_apply_btn.click()
@@ -56,6 +59,42 @@ if state:
 
     phone_n = inputs[-1]
     phone_n.send_keys("09774249341")
+
+    form_footer = chrome_driver.find_element(By.CSS_SELECTOR, value="form footer")
+    next_btn = form_footer.find_element(By.TAG_NAME, value="button")
+    next_btn.click()
+
+    t.sleep(random.uniform(5,9))
+
+    # review
+    form_footer = chrome_driver.find_element(By.CSS_SELECTOR, value="form footer")
+    review_btn = form_footer.find_elements(By.TAG_NAME, value="button")
+
+    if review_btn[1].text != "Review":
+        review_btn[1].click()
+    else:
+        t.sleep(2)
+        easy_apply_modal = chrome_driver.find_element(by=By.CLASS_NAME, value="jobs-easy-apply-modal")
+        buttons = easy_apply_modal.find_elements(By.TAG_NAME, "button")
+        
+        t.sleep(random.uniform(4,8))
+        # exit 
+        buttons[0].click()
+
+
+        confirm_modal = chrome_driver.find_element(by=By.CLASS_NAME, value="artdeco-modal--layer-confirmation")
+        confirm_btn = confirm_modal.find_elements(By.TAG_NAME, "button")
+        
+        t.sleep(random.uniform(4,8))
+        # discard
+        confirm_btn[1].click()
+        print("Skip")
+
+    # submit application
+    # form_footer = chrome_driver.find_element(By.CSS_SELECTOR, value="form footer")
+    # submit_btn = form_footer.find_element(By.TAG_NAME, value="button")
+    # submit_btn.click()
+
 
 else:
     print("Exception occurs")
