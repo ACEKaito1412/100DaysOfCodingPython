a4d9e2fddeae9149d320ca3d9936de602a23f83b
ACEKaito1412 <jhuncarlomacdon@gmail.com>
2025-04-29 22:35:49 +0800

    day 63

--- day_63/database/main.py
+++ day_63/database/main.py
@@ -1,10 +1,33 @@
-import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase
 
-db = sqlite3.connect("books-collecion.db")
+class Base(DeclarativeBase):
+  pass
 
-cursor = db.cursor()
+db = SQLAlchemy(model_class=Base)
 
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+# initialize the app with the extension
+db.init_app(app)
 
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+
+class Books(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[int] = mapped_column(float, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+
+with app.app_context():
+    new_book = Books(id=1,title="Harry Potter", author="j.k rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
