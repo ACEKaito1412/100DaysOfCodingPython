6328347cd717abedde89c588675e466555202be1
ACEKaito1412 <jhuncarlomacdon@gmail.com>
2025-06-19 22:13:37 +0800

    update - portfolio day 83

--- day_83/main.py
+++ day_83/main.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
