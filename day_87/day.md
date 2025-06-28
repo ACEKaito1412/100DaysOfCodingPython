c0ff7948e05607f390bbf62daacea9803e8a239e
ACEKaito1412 <jhuncarlomacdon@gmail.com>
2025-06-28 23:35:08 +0800

    init - day 87 breakout game

---
+++ day_87/day.md
@@ -0,0 +1,3 @@
+# Day 87
+    - Python Turtle
+    - Clone of the 80s hit game Breakout
---
+++ day_87/main.py
@@ -0,0 +1,8 @@
+# conditions 
+    # game ends when ball wasnt catch three times
+# paddle at bottom
+    # move left and right
+# moving ball
+    # bounce at wall and the paddle
+# bricks 
+    # destroy when hit by ball
