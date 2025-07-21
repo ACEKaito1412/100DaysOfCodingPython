127bce732d8f975d4c54ea0bc1a240f8725ee93b
ACEKaito1412 <jhuncarlomacdon@gmail.com>
2025-07-21 20:03:46 +0800

    init - plan day 95

---
+++ day_95/app.py
@@ -0,0 +1,19 @@
+# Using Python Turtle, build the classic shoot 'em up game - space invaders game.
+
+# screen size = 400 / 800
+
+# spaceship - 
+# - > left, right, shoot enemies
+# gameover when life is zero and hit by alien ship
+
+
+# alien ships - 10 * 3 size
+# - move left and right
+# - after a complete movement will go forward
+
+# barriers - - -
+# when hit by bullets it will be destroyed
+# when hit by alien ships will be destroyed
+
+# classes
+# bullets, barriers, alien ships and spaceships
