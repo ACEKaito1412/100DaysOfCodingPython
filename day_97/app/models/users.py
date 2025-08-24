ddd672dd519a3c386910962585d8f12e01a84ef1
ACEKaito1412 <jhuncarlomacdon@gmail.com>
2025-08-24 21:46:20 +0800

    update - order route and user role

--- day_97/app/models/users.py
+++ day_97/app/models/users.py
@@ -1,14 +1,23 @@
 from app import db
-from sqlalchemy import Integer, String, Float, ForeignKey
+from sqlalchemy import Integer, String, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from models.orders import Orders
 from models.carts import Carts
+import enum
+
+
+class RoleEnum(enum.Enum):
+    ADMIN = "admin"
+    USER = "user"
+    MODERATOR = "moderator"
 
 class Users(db.Model):
     id : Mapped[int] = mapped_column(Integer, primary_key=True)
-    name : Mapped[str] = mapped_column(String, nullable=False)
-    email : Mapped[str] = mapped_column(String, nullable=False)
-    password : Mapped[str] = mapped_column(String, nullable=False)
+    name : Mapped[str] = mapped_column(String(100), nullable=False)
+    email : Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
+    password : Mapped[str] = mapped_column(String(200), nullable=False)
+
+    role : Mapped[RoleEnum] = mapped_column(Enum(RoleEnum), default=RoleEnum.USER, nullable=False)
 
     orders : Mapped[list["Orders"]] = relationship(
         "Orders", back_populates="user", cascade="all delete-orphan" 
--- day_97/app/routes/orders.py
+++ day_97/app/routes/orders.py
@@ -1,8 +1,68 @@
 from flask import Blueprint, request, jsonify
-
+from models.orders import OrderItem, Orders
+from app import db
 
 orders_bp = Blueprint('orders', __name__)
 
-@orders_bp.route('/orders', methods=['POST', 'GET'])
-def orders():
-    return 'ok'
+# GET ALL ORDERS
+@orders_bp.route('/', methods=["GET"])
+def get_orders():
+    orders = Orders.query.all()
+
+    return jsonify([
+        {
+            "id" : o.id,
+            "user_id" : o.user_id,
+            "items" : [
+                {"product_id" : i.product_id, "quantity" : i.quantity}
+                for i in o.items
+            ]
+        }
+        for o in orders
+    ]), 200
+
+# GET ORDER BY ID
+@orders_bp.route("/<int:order_id>", methods=["GET"])
+def get_order(order_id):
+    order = Orders.query.get_or_404(order_id)
+    return jsonify({
+        "id" : order.id,
+        "user_id" : order.user_id,
+        "items" : [
+            {"product_id" : i.product_id, "quantity" : i.quantity}
+            for i in order.items
+        ]
+    }), 200
+
+
+# CREATE NEW ORDER
+@orders_bp.route("/", methods=["POST"])
+def create_order():
+    data = request.get_json()
+
+    if not data or "user_id" not in data or "items" not in data:
+        return jsonify({"error" : "Invalid input"}), 400
+    
+    order = Orders(user_id = data["user_id"])
+    for item in data["items"]:
+        order_item = OrderItem(
+            product_id = item["product_id"],
+            quantity = item["quantity", 1],
+            order = order
+        )
+
+        db.session.add(order_item)
+
+    db.session.add(order)
+    db.session.commit()
+
+    return jsonify({"message" : "Order created", "id" : order.id}), 201
+
+# DELETE ORDER
+@orders_bp.route("/<int:order_id>", methods=["DELETE"])
+def delete_order(order_id):
+    order = Orders.query.get_or_404(order_id)
+    db.session.delete(order)
+    db.session.commit()
+
+    return jsonify({"message" : "Order deleted"}), 200
